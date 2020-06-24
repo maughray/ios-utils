c7b49c4afa187de396e360401d758fa3d1722c84
Victor Morei <mcore91@yandex.ru>
2020-06-24 15:32:01 +0300

    Instantiable Storyboard

---
+++ Instantiable.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+protocol Instantiable {
+    static func instantiate() -> Self
+}
+
+extension Instantiable where Self: UIViewController
+{
+    /*
+        Storyboard name should be the same as View Controller
+        Ex: FirstViewController -> First
+    */
+ 
+    static func instantiate() -> Self
+    {
+        let fullName = NSStringFromClass(self)
+        let className = fullName.components(separatedBy: ".")[1]
+        let storyboardName = className.replacingOccurrences(of: "ViewController", with: "")
+        let storyboard = UIStoryboard(name: storyboardName, bundle: Bundle.main)
+        return storyboard.instantiateViewController(withIdentifier: className) as! Self
+    }
+}
